3f4635616441478dc63f36ff26c2065f6cacc34f
Perezonance <53836343+Perezonance@users.noreply.github.com>
2019-08-07 14:53:11 -0400

    Add files via upload

---
+++ Assignment_3.java
@@ -0,0 +1,20 @@
+package com.AManuel.Perez;
+
+import java.util.Scanner;
+
+public class Assignment_3 {
+	public static void main(String[] args) {
+		
+		System.out.println("Bitwise AND of 0, 0: " + (0 & 0));
+		System.out.println("Bitwise AND of 0, 1: " + (0 & 1));
+		System.out.println("Bitwise AND of 1, 0: " + (1 & 0));
+		System.out.println("Bitwise AND of 1, 1: " + (1 & 1));
+		
+		System.out.println();
+		
+		System.out.println("Logical AND of 0, 0: " + (0 & 0));
+		System.out.println("Logical AND of 0, 1: " + (0 & 1));
+		System.out.println("Logical AND of 1, 0: " + (1 & 0));
+		System.out.println("Logical AND of 1, 1: " + (1 & 1));
+	}
+}
